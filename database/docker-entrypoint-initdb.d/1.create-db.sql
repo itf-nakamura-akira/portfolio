de28c9795aeeb31211afcb6f0dc175045da4e121
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-03-02 14:40:26 +0000

    テスト用DBの準備

--- database/docker-entrypoint-initdb.d/1.create-db.sql
+++ database/docker-entrypoint-initdb.d/1.create-db.sql
@@ -1,3 +1,11 @@
+-- 開発用DB
 DROP DATABASE IF EXISTS portfolio;
 
 CREATE DATABASE portfolio;
+
+-- テスト実行用DB
+DROP DATABASE IF EXISTS portfolio_test;
+
+CREATE DATABASE portfolio_test;
+
+GRANT ALL ON portfolio_test.* TO 'itf-user';
--- database/re-create.sh
+++ database/re-create.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
 
 DATABASE=portfolio
+DATABASE_TEST=portfolio_test
 
 mysql --defaults-extra-file=./user.cnf $DATABASE < ./docker-entrypoint-initdb.d/1.create-db.sql
+
 mysql --defaults-extra-file=./user.cnf $DATABASE < ./docker-entrypoint-initdb.d/2.create-table.sql
+mysql --defaults-extra-file=./user.cnf $DATABASE_TEST < ./docker-entrypoint-initdb.d/2.create-table.sql
+
 mysql --defaults-extra-file=./user.cnf $DATABASE < ./docker-entrypoint-initdb.d/3.insert-data.sql
+mysql --defaults-extra-file=./user.cnf $DATABASE_TEST < ./docker-entrypoint-initdb.d/3.insert-data.sql
