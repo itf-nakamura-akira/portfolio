0d2430e60ca04b8e6c5cc6fb979be3e6ac38e29a
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-03-02 14:40:49 +0000

    テスト用DBに接続するようにテストコードを修正

--- backend/portfolio/src/test/java/dev/yukikaze/portfolio/mappers/UsersMapperTests.java
+++ backend/portfolio/src/test/java/dev/yukikaze/portfolio/mappers/UsersMapperTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 
 import dev.yukikaze.portfolio.entities.UsersEntity;
 
@@ -15,6 +16,7 @@ import dev.yukikaze.portfolio.entities.UsersEntity;
  * UsersMapper のテストコード
  */
 @MybatisTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsersMapperTests {
     /**
