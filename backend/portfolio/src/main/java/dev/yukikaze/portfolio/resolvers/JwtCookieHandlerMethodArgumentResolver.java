1e87ed131ec0206561b564b5ddd16a40236e7e70
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-03-06 10:26:47 +0000

    JWTからCookieを取得するアノテーションを実装

---
+++ backend/portfolio/src/main/java/dev/yukikaze/portfolio/resolvers/JwtCookieHandlerMethodArgumentResolver.java
@@ -0,0 +1,47 @@
+package dev.yukikaze.portfolio.resolvers;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.util.WebUtils;
+
+import dev.yukikaze.portfolio.Constants;
+import dev.yukikaze.portfolio.annotations.JwtCookie;
+import dev.yukikaze.portfolio.utils.JwtUtils;
+
+/**
+ * JwtCookieアノテーションのResolver
+ */
+@Component
+public class JwtCookieHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+    private JwtUtils jwtUtils;
+
+    public JwtCookieHandlerMethodArgumentResolver(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer container,
+            NativeWebRequest request, WebDataBinderFactory factory) throws Exception {
+        HttpServletRequest httpServletRequest = request.getNativeRequest(HttpServletRequest.class);
+        Cookie jwtCookie = WebUtils.getCookie(httpServletRequest, Constants.JWT_COOKIE_KEY);
+
+        if (jwtCookie != null) {
+            return this.jwtUtils.verifyToken(jwtCookie.getValue()).orElse(null);
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasMethodAnnotation(JwtCookie.class)
+                || parameter.hasParameterAnnotation(JwtCookie.class);
+    }
+}
