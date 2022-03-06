2b4fec646219586a1884fd176cb9987b728ee1be
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-03-06 14:18:55 +0000

    Jestの導入・Karmaの排除

---
+++ frontend/jest.config.js
@@ -0,0 +1,6 @@
+// jest.config.js
+module.exports = {
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setupJest.ts'],
+    globalSetup: 'jest-preset-angular/global-setup',
+};
