8f6657836fe329198ab36911225621b8ac0ec8d2
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-03-08 10:05:08 +0000

    jestの設定を修正

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
     globalSetup: 'jest-preset-angular/global-setup',
+    testMatch: ['<rootDir>/src/**/+(*.)+(spec).+(ts)'],
 };
