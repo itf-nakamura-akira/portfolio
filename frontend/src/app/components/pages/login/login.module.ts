d18adff7cf64ee88675c36489d2e734b4ab3c726
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-03-11 14:56:14 +0000

    不要なモジュールimportを削除

--- frontend/src/app/components/pages/login/login.module.ts
+++ frontend/src/app/components/pages/login/login.module.ts
@@ -4,7 +4,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { LoginRoutingModule } from './login-routing.module';
@@ -16,7 +15,6 @@ import { TestUserBottomSheetComponent } from './test-user-bottom-sheet/test-user
     imports: [
         CommonModule,
         ReactiveFormsModule,
-        MatFormFieldModule,
         MatInputModule,
         MatButtonModule,
         MatCardModule,
