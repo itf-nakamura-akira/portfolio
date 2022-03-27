251c3499711c050acb81cab8d81c686fe7664f9f
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-03-27 17:42:28 +0000

    勤怠入力画面のComponentを追加

--- frontend/src/app/components/pages/input-working-hours/input-working-hours.module.ts
+++ frontend/src/app/components/pages/input-working-hours/input-working-hours.module.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 import { InputWorkingHoursRoutingModule } from './input-working-hours-routing.module';
 import { InputWorkingHoursComponent } from './input-working-hours.component';
+import { ListComponent } from './list/list.component';
+import { SearchComponent } from './header/search/search.component';
+import { ActionComponent } from './header/action/action.component';
 
 @NgModule({
-    declarations: [InputWorkingHoursComponent],
+    declarations: [InputWorkingHoursComponent, HeaderComponent, ListComponent, SearchComponent, ActionComponent],
     imports: [CommonModule, InputWorkingHoursRoutingModule],
 })
 export class InputWorkingHoursModule {}
